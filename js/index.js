0befe89ea7a6f4abf1b06a8338355629f39092b2
Taisiia <funnytaya@gmail.com>
2023-01-31 21:02:25 +0200

    HW9 added

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-// import {items} from "./items.js"
 
 // Swiper
 new Swiper('.swiper', {
@@ -19,15 +18,50 @@ document.querySelector("#search-input").addEventListener("keyup", function() {
   }
 });
 
-// const cardContainer = document.querySelector(".cards-items")
+// ////////////////  Accordion
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+// Modal close
+document.querySelector('.modal').addEventListener("click", (event) => {
+	const isClickInsideModal = document.querySelector(".modal_active .innerModal").contains(event.target)
+	if (!isClickInsideModal) {
+		document.querySelector(".modal_active").classList.remove('modal_active')
+	}
+});
 
 
 const createCard = (itemList) => {
-	const cardContainer = document.querySelector(".cards-items")
+	const cardContainer = document.querySelector(".cards-items");
+	
 	itemList.forEach(element => {
 		const cardElement = document.createElement('div'); 
+		
 		console.log(element);
 		cardElement.classList.add('card');
+
+		// Modal open
+		cardElement.onclick = () => {
+			
+			createModal(element)
+		};
+
 		///////// card-info
 		const cardInfo = document.createElement('div');
 		cardInfo.classList.add('card-info');
@@ -71,7 +105,6 @@ const createCard = (itemList) => {
 
 		cardInfo.appendChild(itemPrice)
 		itemPrice.appendChild(priceNumber)
-
 
 		//buttonCard
 		const buttonCard = document.createElement('button')
@@ -147,6 +180,65 @@ const createCard = (itemList) => {
 
 		// добавляем в карточку
 		cardContainer.appendChild(cardElement);
-		
 	});
 } 
+
+const createModal = (item) => {
+	// Modal
+	const modalElem = document.querySelector(".modal");
+	console.log(item);
+
+	modalElem.classList.add('modal_active');
+
+	//name
+	const modalName = document.querySelector('.name-modal');
+	modalName.textContent = item.name;
+
+	//likes
+	const likeModal =  document.querySelector('.number-rewiews-modal');
+	const likes = Math.floor(Math.random() * 101)
+	likeModal.textContent = `${likes}%`;
+	//orders
+	const orderModel = document.querySelector('.number-of-orders-modal');
+	const ordered = Math.floor(Math.random() * 1001)
+	orderModel.textContent = `${ordered}`;
+	
+
+	//full information about item
+	const imgModal = document.querySelector('.img-modal');
+	imgModal.src = `./img/${item.imgUrl}`;
+
+	const color = document.querySelector('.description-color');
+	color.textContent = item.color.join(', ');
+
+	const os = document.querySelector('.description-os');
+	os.textContent = item.os;
+
+	//chip
+	const chip = document.querySelector('.description-chip');
+
+	chip.textContent = item.chip.cores === null ? `${item.chip.name}`: `${item.chip.name} cores: ${item.chip.cores}`
+
+	const height = document.querySelector('.description-height')
+	height.textContent = item.size.height;
+
+	const width = document.querySelector('.description-width');
+	width.textContent = item.size.width;
+
+	const depth = document.querySelector('.description-depth');
+	depth.textContent = item.size.depth;
+
+	const weight = document.querySelector('.description-weight');
+	weight.textContent = item.size.weight;
+
+	// price
+	const priceInModal = document.querySelector('.price-modal-number')
+	priceInModal.textContent = item.price;
+
+	//in stock
+	const inStockModal = document.querySelector('.number-in-stock-modal')
+	inStockModal.textContent = item.orderInfo.inStock;
+	//btn 
+	// const btnModal = document.querySelector('.btn-modal');
+	// btnModal.textContent = "Add to cart";
+}
