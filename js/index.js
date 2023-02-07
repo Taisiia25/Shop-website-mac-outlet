990ca320a30b1236deae08e3edaf9efb9abf7214
Taisiia <funnytaya@gmail.com>
2023-02-07 19:18:08 +0200

    HW10 added

--- js/index.js
+++ js/index.js
@@ -1,42 +1,14 @@
 
 // Swiper
 new Swiper('.swiper', {
-    slidesPerGroup: 1,
-
-    centerSliders: true,
-});
-
-/////////////// input
-
-document.querySelector("#search-input").addEventListener("keyup", function() {
-	var searchbox = document.querySelector(".aa-input");
-	var reset = document.querySelector(".searchbox [type='reset']");
-	if (searchbox.value.length === 0){
-		reset.classList.add("hide");
-	} else {
-	  reset.classList.remove('hide');
-  }
-});
-
-// ////////////////  Accordion
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-    this.classList.toggle("active");
-
-    /* Toggle between hiding and showing the active panel */
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
+	autoplay: {
+	  delay: 3000,
+	  stopOnLastSlide: false,
+	  disableOnIteraction: false,
+	},
+	speed: 800,
   });
-}
+
 
 // Modal close
 document.querySelector('.modal').addEventListener("click", (event) => {
@@ -46,142 +18,6 @@ document.querySelector('.modal').addEventListener("click", (event) => {
 	}
 });
 
-
-const createCard = (itemList) => {
-	const cardContainer = document.querySelector(".cards-items");
-	
-	itemList.forEach(element => {
-		const cardElement = document.createElement('div'); 
-		
-		console.log(element);
-		cardElement.classList.add('card');
-
-		// Modal open
-		cardElement.onclick = () => {
-			
-			createModal(element)
-		};
-
-		///////// card-info
-		const cardInfo = document.createElement('div');
-		cardInfo.classList.add('card-info');
-		cardElement.appendChild(cardInfo);
-		//img-card
-		const cardImg = document.createElement('div'); 
-		cardImg.classList.add('img-card');
-		const imgPicture = document.createElement('img');
-		imgPicture.classList.add('img-card'); 
-		imgPicture.src = `./img/${element.imgUrl}`
-		
-		cardImg.appendChild(imgPicture);
-		cardInfo.appendChild(cardImg);
-
-		//item-name
-		const itemName = document.createElement('p');
-		itemName.classList.add('item-name');
-		itemName.textContent = element.name;
-		cardInfo.appendChild(itemName);
-		//stock
-		const itemStock = document.createElement('div');
-		itemStock.classList.add('stock');
-		const itemInStock = document.createElement('div');
-		itemInStock.textContent = "left in stock";
-		itemInStock.classList.add('in-stock');
-		const numberInStock = document.createElement('span');
-		numberInStock.classList.add('number-in-stock')
-		numberInStock.textContent = element.orderInfo.inStock;
-		
-		itemInStock.prepend(numberInStock);
-		itemStock.appendChild(itemInStock);
-		cardInfo.appendChild(itemStock)
-
-		//price
-		const itemPrice = document.createElement('div');
-		itemPrice.classList.add('price');
-		itemPrice.textContent = "Price: $";
-		const priceNumber = document.createElement('span');
-		priceNumber.classList.add('price-number');
-		priceNumber.textContent = element.price;
-
-		cardInfo.appendChild(itemPrice)
-		itemPrice.appendChild(priceNumber)
-
-		//buttonCard
-		const buttonCard = document.createElement('button')
-		buttonCard.classList.add('btn','btn-card')
-		buttonCard.textContent = "Add to cart";
-		cardInfo.appendChild(buttonCard)
-		//likeButton
-		const likeButton = document.createElement('button')
-		likeButton.classList.add('like-btn')
-		const likeImg = document.createElement('img')
-		likeImg.classList.add('like')
-		likeImg.src = "img/icons/like_filled1.png"
-		likeButton.appendChild(likeImg)
-		cardInfo.appendChild(likeButton)
-
-		///////card-footer
-		const cardFooter = document.createElement('div');
-		cardFooter.classList.add('card-footer');
-		cardElement.appendChild(cardFooter);
-		//like-permanent
-		const likePermanent = document.createElement('div'); 
-		likePermanent.classList.add('like-permanent');
-		const imgLike = document.createElement('img');
-		imgLike.classList.add('like-permanent'); 
-		imgLike.src = "img/icons/like_filled1.png";
-
-		likePermanent.appendChild(imgLike);
-		cardFooter.appendChild(likePermanent);
-
-		//reviews
-		const itemRewiews = document.createElement('div');
-		itemRewiews.classList.add('reviews');
-
-		const textReview = document.createElement('p');
-		textReview.classList.add('text-rewiew');
-		textReview.textContent = " Positive reviews";
-		//number-rewiews
-		const numberRewiews = document.createElement('span');
-		numberRewiews.classList.add('number-rewiews');
-		const likes = Math.floor(Math.random() * 101)
-		numberRewiews.textContent = `${likes}%`;
-		
-		const reviewAbove = document.createElement('p');
-		reviewAbove.classList.add('reviews-above');
-		reviewAbove.textContent = " Above avarage";
-		
-
-		cardFooter.appendChild(itemRewiews)
-		itemRewiews.appendChild(textReview)
-		textReview.prepend(numberRewiews)
-		itemRewiews.appendChild(reviewAbove)
-
-		//orders
-
-		const orders = document.createElement('div');
-		orders.classList.add('orders');
-
-		const numberOrders = document.createElement('p');
-		numberOrders.classList.add('number-of-orders');
-		const ordered = Math.floor(Math.random() * 1001)
-		numberOrders.textContent = `${ordered}`;
-
-		const orderText = document.createElement('p');
-		orderText.classList.add('orders-text');
-		orderText.textContent = "orders";
-
-
-
-		cardFooter.appendChild(orders)
-		orders.appendChild(numberOrders)
-		orders.appendChild(orderText)
-		
-
-		// добавляем в карточку
-		cardContainer.appendChild(cardElement);
-	});
-} 
 
 const createModal = (item) => {
 	// Modal
@@ -232,13 +68,490 @@ const createModal = (item) => {
 	weight.textContent = item.size.weight;
 
 	// price
-	const priceInModal = document.querySelector('.price-modal-number')
+	const priceInModal = document.querySelector('.price-modal-number');
 	priceInModal.textContent = item.price;
 
 	//in stock
-	const inStockModal = document.querySelector('.number-in-stock-modal')
+	const inStockModal = document.querySelector('.number-in-stock-modal');
 	inStockModal.textContent = item.orderInfo.inStock;
 	//btn 
 	// const btnModal = document.querySelector('.btn-modal');
 	// btnModal.textContent = "Add to cart";
 }
+
+class Item {
+	constructor(item) {
+		Object.assign(this, item);
+		this.like = false;
+		this.orders = Math.round(Math.random() * 1000);
+		this.likes = Math.floor(Math.random() * 101)
+	}
+
+	get isAvailableForBuy(){
+		return this.orderInfo.inStock > 0;
+	}
+
+	get absoluteImgPath(){
+		return `img/${this.imgUrl}`;
+	}
+
+	toggleLike(){
+		//to rewrite current value
+		return this.like = !this.like;
+	}
+
+	checkIsNameIncludes(name) {
+        const nameAsLowerCase = name.toLowerCase();
+        return this.name.toLowerCase().includes(nameAsLowerCase);
+    }
+
+	checkIsColorIncludes(colors) {
+        if(!colors.length) return true;
+
+        for(const color of colors) {
+            const isExists = this.color.includes(color);
+            if(isExists) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    checkIsStorageIncludes(storages) {
+        // storages [256, 512, 1024]
+        // this.storage 2048
+        if(!storages.length) return true;
+
+        for(const storage of storages) {
+            if(this.storage === storage){
+                return true;
+            }
+        }
+        return false;
+    }
+
+	checkIsOsIncludes(oses) { 
+        if(!oses.length) return true;
+
+        for(const os of oses) {
+            if(this.os === os){
+                return true;
+            }
+        }
+        return false;
+    }
+
+	checkIsDisplayIncludes(displayes) {
+        
+        if(!displayes.length) return true;
+
+        for (const display of displayes) {
+			if (display === '<5' && this.display < 5) {
+			  return true;
+			}
+			if (display === '5-7' && this.display >= 5 && this.display < 7) {
+			  return true;
+			}
+			if (display === '7-12' && this.display >= 7 && this.display < 12) {
+			  return true;
+			}
+			if (display === '12-16' && this.display >= 12 && this.display <= 16) {
+			  return true;
+			}
+			if (display === '+16' && this.display > 16 && this.display !== null) {
+			  return true;
+			}
+		}
+    }
+
+	checkIsPriceIncludes(prices) {
+		if (!prices.length) return true;
+
+		for (const price of prices) {
+		  if (this.price === price) {
+			return true;
+		  }
+		}
+		return false;
+	}
+
+
+}
+//зберігає всі item для запитів(як БД) у вигляді екземплярів класу Item
+class ItemsModel {
+	constructor() { 
+		// Create Item instances list from items list
+		this.items = items.map(item => new Item(item));
+	}
+
+	// array with colors from  item list
+	get availableColors(){
+		return this.items
+		.reduce((acc, item) => [...acc, ...item.color],[])
+		.filter((item, index, arr) => arr.indexOf(item) === index);
+	}
+
+	get availableStorage(){
+		return this.items
+		.map(item => item.storage)
+		.filter((item, index, arr) => arr.indexOf(item) === index && item !== null);
+	}
+
+	get availableOses(){
+		return this.items
+		.map(item => item.os)
+		.filter((item, index, arr) => arr.indexOf(item) === index && item !== null);
+	}
+
+	get availableDisplayes(){
+		let modifiedDisplays = ['<5', '5-7', '7-12', '12-16', '+16'];
+    	return modifiedDisplays
+			.filter((item, index, arr) => arr.indexOf(item) === index && item !== null);
+	}
+
+	get availablePrice() {
+		return this.items
+		  .map(item => item.price)
+		  .filter(
+			(item, index, arr) => arr.indexOf(item) === index && item !== null)
+			.sort((a, b) => {return a - b});	
+	}
+
+	// Get list with items based on query as substring in item name
+	findManyByName(name) {
+        const nameAsLowerCase = name.toLowerCase();
+        return this.items.filter(item => item.name.toLowerCase().includes(nameAsLowerCase));
+	}
+
+	filterItems(filter = {}) {
+		const {
+			name = '',
+			color = [],
+			storage = [],
+			os = [],
+			display = [],
+			price = [],
+		} = filter;
+
+		return this.items.filter(item => {
+            // Check on substring includes in string
+            const isNameIncluded = item.checkIsNameIncludes(name);
+            if(!isNameIncluded) return false;
+
+            // Check on substring includes in string
+            const isColorIncluded = item.checkIsColorIncludes(color);
+            if(!isColorIncluded) return false;
+
+            // Check on substring includes in string
+            const isStorageIncluded = item.checkIsStorageIncludes(storage);
+            if(!isStorageIncluded) return false;
+
+			const isOsIncluded = item.checkIsOsIncludes(os);
+            if(!isOsIncluded) return false;
+
+			const isDisplayIncluded = item.checkIsDisplayIncludes(display);
+            if(!isDisplayIncluded) return false;
+
+			const isPriceIncluded = item.checkIsPriceIncludes(price);
+      		if (!isPriceIncluded) return false;
+
+			return true;
+		});
+	}
+}
+
+class RenderCards {
+	//  seems like dependancy injection ?
+	constructor(itemsModel) {
+		this.cardsContainer = document.querySelector(".cards-items"); // Container  element
+		this.renderCards(itemsModel.items); // Auto render cards after init page
+	}
+	
+	static renderCard(item)  {
+
+		const cardElement = document.createElement('div'); 
+		
+		cardElement.className = 'card';
+
+		// Modal open
+		cardElement.onclick = () => {
+			
+			createModal(item)
+		};
+
+
+		// Add content
+		cardElement.innerHTML = `
+		<div  class="card-info">
+                                <img class="img-card" src="${item.absoluteImgPath}"/>
+                                <p class="item-name">${item.name}</p>
+                                <div class="stock card-availability">${item.orderInfo.inStock} left in stock</div>
+                                <div class="price">Price: <span class="price price-number">${item.price}</span> $</div>
+                                <button class="btn btn-card">Add to cart</button>
+                                <button class="like-btn">
+                                    <img class="like" src="img/icons/like_filled1.png">
+                                </button>
+                            </div>
+                            <div class="card-footer">
+                                <div class="like-permanent">
+                                    <img src="img/icons/like_filled1.png">
+                                </div>
+                                <div class="reviews">
+                                    <p class="reviews text-rewiew"><span class="reviews number-rewiews">${item.likes}%</span>Positive reviews</p>
+                                    <p class="reviews reviews-above"> Above avarage</p>
+                                </div>
+                                <div class="orders">
+                                    <p class="number-of-orders">${item.orders}</p>
+                                    <p class="orders-text">orders</p>
+                                </div>
+                            </div>
+		`;
+
+		const likeBtn = cardElement.querySelector('.like');
+		// for saving current condition for likeBtn if card will be rerendered
+		if(item.like) {
+			likeBtn.classList('active');
+		}
+
+		likeBtn.onclick = () => {
+			item.toggleLike();
+			likeBtn.classList.toggle('active');
+		}
+
+		likeBtn.addEventListener('click', e => {
+			likeBtn.classList.toggle('like_active');
+			e.stopPropagation();
+		});
+
+		const button = cardElement.querySelector('.btn');
+    	const iconDisable = cardElement.querySelector('.card-availability');
+
+		if (item.isAvailableForBuy) {
+			button.addEventListener('click', e => {
+				e.stopPropagation();
+			});
+		} else {
+			button.disabled = true;
+			button.classList.add('disable-btn');
+			iconDisable.classList.add('card-non-availability');
+		}
+
+		return cardElement;
+	}
+	
+
+	renderCards(items) {
+        // Clear container
+        this.cardsContainer.innerHTML = '';
+
+        // Cereate elements with cards based on items list
+        const elements = items.map(item => RenderCards.renderCard(item));
+
+        // Add elements to container
+        this.cardsContainer.append(...elements);
+    }
+}
+
+
+class Filter {
+	#itemsModel = null;
+	#renderCards = null;
+	constructor(itemsModel, renderCards){
+		this.name = '';
+		this.sort = 'default';
+		this.color = [];
+		this.storage = [];
+		this.os = [];
+		this.display = [];
+		this.from = 0;
+    	this.to = Infinity;
+		this.#itemsModel = itemsModel;
+		this.#renderCards = renderCards;
+	}
+	// для розуміння який фільтер використовується (sort/name)
+	setFilter(key, value) {
+		if (!Array.isArray(this[key])) {
+			this[key] = value;
+			this.#findAndRerender();
+			console.log(this)
+			return;
+		}
+
+		if(this[key].includes(value)) {
+			// якщо колір є, то він видалиться
+			this[key] = this[key].filter(val => val !== value);
+		} else {
+			// якщо немає. то додається
+			this[key].push(value);
+		}
+		this.#findAndRerender();
+	}
+
+	#findAndRerender() {
+		const items = this.#itemsModel.filterItems({...this});
+		
+		this.#renderCards.renderCards(items);
+	}
+}
+
+class RenderFilters {
+	#filter = null;
+	constructor(itemsModel, filter){
+		this.#filter = filter;
+		this.containerElem = document.querySelector('.accordion-container');
+		this.filterOptions = [
+			{
+				displayName: 'Price',
+				name: 'price',
+				options: itemsModel.availablePrice,
+			  },
+			{
+				displayName: 'Color',
+				name: 'color',
+				options: itemsModel.availableColors,
+			},
+			{
+				displayName: 'Storage',
+				name: 'storage',
+				options: itemsModel.availableStorage,
+			},
+			{
+				displayName: 'OS',
+				name: 'os',
+				options: itemsModel.availableOses,
+			},
+			{
+				displayName: 'Display',
+				name: 'display',
+				options: itemsModel.availableDisplayes,
+			}
+		];
+
+		this.inputName = document.querySelector('.searchbox__input');
+		this.selectSort = document.getElementById('sortFilter');
+
+		// виклик setFilter (запис з input)
+		this.inputName.oninput = (event)  =>  {
+			const { value }  = event.target;
+			this.#filter.setFilter('name', value);
+		}
+		// value = asc/desc/default
+		this.selectSort.onchange = (event)  =>  {
+			const { value }  = event.target;
+			console.log(value);
+			this.#filter.setFilter('sort', value);
+		}
+
+		this.renderFilters(this.filterOptions);
+	}
+
+	renderFilter(optionsData){
+		const filterAccordionBtn = document.createElement('button');
+		filterAccordionBtn.className = 'accordion';
+		filterAccordionBtn.innerHTML = `<p class="accordion-title-text">${optionsData.displayName}</p>
+		<img class="accordion-icon" src="img/icons/arrow_left.svg" alt=""/>`;
+		this.containerElem.append(filterAccordionBtn);
+
+		const filterPanel = document.createElement('div');
+		filterPanel.className = 'panel-accordion';
+
+		if (optionsData.name !== 'price') {
+			const optionsElements = optionsData.options.map(option => {
+			const filterOption = document.createElement('label');
+			filterOption.innerHTML = `<span>${option}</span>`;
+
+			const checkbox = document.createElement('input');
+			checkbox.type = 'checkbox';
+			checkbox.value = option;
+			checkbox.onchange = () => {
+				this.#filter.setFilter(optionsData.name, option);
+			};
+			filterOption.appendChild(checkbox);
+
+			return filterOption;
+			});
+
+			filterPanel.append(...optionsElements);
+			this.containerElem.append(filterPanel);
+		}
+
+		if (optionsData.name === 'price') {
+			const minPrice = document.createElement('label');
+			const maxPrice = document.createElement('label');
+			const inputMinPrice = document.createElement('input');
+			const inputMaxPrice = document.createElement('input');
+			const inputPriceFrom = document.createElement('p');
+			const inputPriceTo = document.createElement('p');
+			filterPanel.className = 'panel-accordion';
+			filterPanel.className = ' panel-price-accordion';
+			inputMinPrice.className = 'input-min-price';
+			inputMaxPrice.className = 'input-max-price';
+			inputMinPrice.type = 'number';
+            inputMaxPrice.type = 'number';
+
+			inputPriceFrom.innerHTML = 'From:';
+			inputPriceTo.innerHTML = 'To:'
+
+			inputPriceFrom.style.fontSize = '16px';
+			inputPriceTo.style.fontSize = '16px';
+
+			minPrice.append(inputPriceFrom, inputMinPrice);
+			maxPrice.append(inputPriceTo, inputMaxPrice);
+
+			filterPanel.append(minPrice, maxPrice);
+			this.containerElem.append(filterPanel);
+		}
+
+		filterAccordionBtn.addEventListener('click', function () {
+			this.classList.toggle('active');
+		
+			const panel = this.nextElementSibling;
+			if (panel.style.display === 'block') {
+				panel.style.display = 'none';
+			} else {
+				panel.style.display = 'block';
+			}
+		});
+
+		return filter;
+	}
+	
+
+	renderFilters(){
+		this.containerElem.innerHTML = '';
+
+		const filtersElements = this.filterOptions.map(optionsData =>
+			this.renderFilter(optionsData)
+			);
+
+		return filtersElements;
+	}
+}
+
+
+const itemsModel = new ItemsModel();
+const renderCards = new RenderCards(itemsModel);
+const filter = new Filter(itemsModel, renderCards);
+const renderFilters = new RenderFilters(itemsModel, filter)
+
+
+
+
+// inputName.oninput = (event)  =>  {
+// 	const fondItems = itemsModel.findManyByName(event.target.value);
+// 	renderCards.renderCards(fondItems);
+// }
+
+
+
+/////////////// input
+
+// document.querySelector("#search-input").addEventListener("keyup", function() {
+// 	var searchbox = document.querySelector(".aa-input");
+// 	var reset = document.querySelector(".searchbox [type='reset']");
+// 	if (searchbox.value.length === 0){
+// 		reset.classList.add("hide");
+// 	} else {
+// 	  reset.classList.remove('hide');
+//   }
+// });
